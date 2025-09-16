2cae99904bf94ea4373e9c5ef43e4e0f87bcc561
Dari <dariatsevashova@gmail.com>
2025-09-16 09:07:51 +0200

    hw2

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,7 @@
+import notifCss from "../Notification/Notification.module.css";
+
+const Notification = () => {
+  return <p className={notifCss.message}> No feedback yet</p>;
+};
+
+export default Notification;
